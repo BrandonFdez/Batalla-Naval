14dc09c97f3b02b0301232ee83bcb7ab23f599dd
BrandonFdez <drawboy10@gmail.com>
2024-11-07 14:35:10 -0500

    Genesis

--- src/main/java/com/example/batallanaval/Main.java
+++ src/main/java/com/example/batallanaval/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Abre la ventana de inicio
+        // Abre la ventana de inicio.
         new BNFirsStage();
     }
 }
