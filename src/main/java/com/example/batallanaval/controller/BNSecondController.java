3f1385f9068e371e9bb86ddcae8e8be73511fc4d
BrandonFdez <drawboy10@gmail.com>
2024-11-14 20:44:30 -0500

    Rotación

--- src/main/java/com/example/batallanaval/controller/BNSecondController.java
+++ src/main/java/com/example/batallanaval/controller/BNSecondController.java
@@ -16,8 +16,8 @@ public class BNSecondController {
     private Rectangle Submarine;  // Rectangulo para los Submarinos
     private Rectangle AircraftCarrier;  // Rectangulo para los portaaviones
 
-    // Metodo que permite arrastrar cualquier nodo
-    private void MakeDraggable(Node Node) {
+    // Metodo que permite arrastrar cualquier nodo y rotarlo
+    private void MakeDraggableAndRotatable(Node Node) {
         final double[] DragCoordinates = new double[2]; // Este arreglo permite recordar el desplazamiento entre la posición del nodo y la posición del mouse al hacer clic en el nodo
 
         Node.setOnMousePressed(event -> {
@@ -28,6 +28,18 @@ public class BNSecondController {
         Node.setOnMouseDragged(event -> {
             Node.setLayoutX(event.getSceneX() + DragCoordinates[0]);
             Node.setLayoutY(event.getSceneY() + DragCoordinates[1]);
+        });
+
+        // Evento para rotar el nodo al hacer clic derecho
+        Node.setOnMouseClicked(event -> {
+            if (event.isPrimaryButtonDown()) { // Verifica si es clic derecho
+                // Cambia la rotación entre 0 y 90 grados
+                if (Node.getRotate() == 0) {
+                    Node.setRotate(90); // Rotación vertical
+                } else {
+                    Node.setRotate(0); // Rotación horizontal
+                }
+            }
         });
     }
 
@@ -46,10 +58,10 @@ public class BNSecondController {
         // Añadir los rectángulos al Pane
         MyPane.getChildren().addAll(Frigate, Destroyer, Submarine, AircraftCarrier);
 
-        // Llama a MakeDraggable para hacer los objetos arrastrables
-        MakeDraggable(Frigate);
-        MakeDraggable(Destroyer);
-        MakeDraggable(Submarine);
-        MakeDraggable(AircraftCarrier);
+        // Llama a MakeDraggableAndRotatable para hacer los objetos arrastrables
+        MakeDraggableAndRotatable(Frigate);
+        MakeDraggableAndRotatable(Destroyer);
+        MakeDraggableAndRotatable(Submarine);
+        MakeDraggableAndRotatable(AircraftCarrier);
     }
 }
